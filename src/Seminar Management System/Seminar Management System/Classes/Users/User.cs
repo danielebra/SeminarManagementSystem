061121fa4accc1f435f87d23762eefa4d727b66d
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-08-05 13:09:49 +1000

    Create basic User classes

---
+++ src/Seminar Management System/Seminar Management System/Classes/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_Management_System.Classes.Users
+{ 
+    // TODO
+    // Decide what class type: abstract or virtual
+
+    class User
+    {
+        public string Name { get; set; }
+    }
+}
