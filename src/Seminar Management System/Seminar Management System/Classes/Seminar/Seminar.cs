faecbcf1f3e0a1b223692d46190a536ec8d7a25b
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-08-05 13:10:01 +1000

    Create basic Seminar classes

---
+++ src/Seminar Management System/Seminar Management System/Classes/Seminar/Seminar.cs
@@ -0,0 +1,18 @@
+﻿using Seminar_Management_System.Classes.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_Management_System.Classes.Seminar
+{
+    class Seminar
+    {
+        public SeminarOrganiser Organiser { get; set; }
+        public List<Speaker> Speakers { get; set; }
+        public List<SeminarAttendee> Attendees { get; set; }
+
+        public string Description { get; set; }
+    }
+}
