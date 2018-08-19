43cb653689bcc103aef6330d4ac031fe39d2792a
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-08-19 15:26:55 +1000

    Implement edit in ViewSeminar

--- src/Seminar Management System/Seminar Management System/Forms/ViewSeminar.Designer.cs
+++ src/Seminar Management System/Seminar Management System/Forms/ViewSeminar.Designer.cs
@@ -28,38 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.lblDescription = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.rtbDescription = new System.Windows.Forms.RichTextBox();
+            this.selectSpeakers1 = new Seminar_Management_System.Custom_Controls.SelectSpeakers();
+            this.ddOrganisers = new Seminar_Management_System.Custom_Controls.OrganiserDropDown();
+            this.ddVenue = new Seminar_Management_System.Custom_Controls.VenueDropDown();
+            this.datePickerSingle = new Seminar_Management_System.DatePickerSingle();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblTitle
+            // btnEdit
             // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(397, 9);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(66, 31);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Title";
+            this.btnEdit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.Location = new System.Drawing.Point(823, 9);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.TextChanged += new System.EventHandler(this.btnEdit_TextChanged);
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
-            // lblDescription
+            // tbTitle
             // 
-            this.lblDescription.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
-            this.lblDescription.AutoSize = true;
-            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescription.Location = new System.Drawing.Point(391, 40);
-            this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(83, 18);
-            this.lblDescription.TabIndex = 0;
-            this.lblDescription.Text = "Description";
+            this.tbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTitle.Location = new System.Drawing.Point(12, 5);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.ReadOnly = true;
+            this.tbTitle.Size = new System.Drawing.Size(775, 40);
+            this.tbTitle.TabIndex = 2;
+            this.tbTitle.Text = "Title";
+            // 
+            // rtbDescription
+            // 
+            this.rtbDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbDescription.Location = new System.Drawing.Point(12, 51);
+            this.rtbDescription.Name = "rtbDescription";
+            this.rtbDescription.ReadOnly = true;
+            this.rtbDescription.Size = new System.Drawing.Size(775, 167);
+            this.rtbDescription.TabIndex = 3;
+            this.rtbDescription.Text = "Description";
+            // 
+            // selectSpeakers1
+            // 
+            this.selectSpeakers1.Enabled = false;
+            this.selectSpeakers1.Location = new System.Drawing.Point(287, 224);
+            this.selectSpeakers1.Name = "selectSpeakers1";
+            this.selectSpeakers1.Size = new System.Drawing.Size(214, 147);
+            this.selectSpeakers1.TabIndex = 16;
+            // 
+            // ddOrganisers
+            // 
+            this.ddOrganisers.Enabled = false;
+            this.ddOrganisers.Location = new System.Drawing.Point(287, 377);
+            this.ddOrganisers.Name = "ddOrganisers";
+            this.ddOrganisers.Size = new System.Drawing.Size(190, 30);
+            this.ddOrganisers.TabIndex = 17;
+            // 
+            // ddVenue
+            // 
+            this.ddVenue.Enabled = false;
+            this.ddVenue.Location = new System.Drawing.Point(287, 413);
+            this.ddVenue.Name = "ddVenue";
+            this.ddVenue.Size = new System.Drawing.Size(190, 30);
+            this.ddVenue.TabIndex = 18;
+            // 
+            // datePickerSingle
+            // 
+            this.datePickerSingle.Enabled = false;
+            this.datePickerSingle.Location = new System.Drawing.Point(12, 224);
+            this.datePickerSingle.Name = "datePickerSingle";
+            this.datePickerSingle.Size = new System.Drawing.Size(269, 249);
+            this.datePickerSingle.TabIndex = 19;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.Red;
+            this.btnCancel.Location = new System.Drawing.Point(823, 38);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // ViewSeminar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(915, 571);
-            this.Controls.Add(this.lblDescription);
-            this.Controls.Add(this.lblTitle);
+            this.ClientSize = new System.Drawing.Size(910, 633);
+            this.Controls.Add(this.datePickerSingle);
+            this.Controls.Add(this.ddVenue);
+            this.Controls.Add(this.ddOrganisers);
+            this.Controls.Add(this.selectSpeakers1);
+            this.Controls.Add(this.rtbDescription);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnEdit);
             this.Name = "ViewSeminar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ViewSeminar";
@@ -70,8 +139,13 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.RichTextBox rtbDescription;
+        private Custom_Controls.SelectSpeakers selectSpeakers1;
+        private Custom_Controls.OrganiserDropDown ddOrganisers;
+        private Custom_Controls.VenueDropDown ddVenue;
+        private DatePickerSingle datePickerSingle;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
--- src/Seminar Management System/Seminar Management System/Forms/ViewSeminar.cs
+++ src/Seminar Management System/Seminar Management System/Forms/ViewSeminar.cs
@@ -24,12 +24,99 @@ namespace Seminar_Management_System.Forms
         }
         private Seminar seminarReference { get; set; }
 
+        private const string EDIT = "Edit";
+        private const string SAVE = "Save";
+        
         private void ViewSeminar_Load(object sender, EventArgs e)
+        {
+            populateDataFields();
+        }
+
+        private void populateDataFields()
         {
             if (seminarReference != null)
             {
-                lblTitle.Text = seminarReference.Title;
-                lblDescription.Text = seminarReference.Description;
+                tbTitle.Text = seminarReference.Title;
+                rtbDescription.Text = seminarReference.Description;
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (btnEdit.Text == EDIT)
+            {
+                enableEditing();
+                btnEdit.Text = SAVE;
+                btnCancel.Visible = true;
+            }
+            else if (btnEdit.Text == SAVE)
+            {
+                disableEditing();
+                btnEdit.Text = EDIT;
+                btnCancel.Visible = false;
+                saveSeminarState();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            populateDataFields();
+            disableEditing();
+            btnCancel.Visible = false;
+            btnEdit.Text = EDIT;
+        }
+
+        private void saveSeminarState()
+        {
+            // TODO
+            // Take value from interface and update the seminarReference to reflect them.
+
+        }
+
+        private void _enableEditing(bool canEdit)
+        {
+            // param canEdit: true refers to enabling on
+            //                false refers to enabling off
+
+            // Forloop can be removed and controls be accessed
+            // directly, once the complexity of the controls
+            // is determined.
+            foreach (var cont in this.Controls)
+            {
+                if (cont.GetType() == typeof(TextBox))
+                {
+                    ((TextBox)cont).ReadOnly = !canEdit;
+                }
+                else if (cont.GetType() == typeof(RichTextBox))
+                {
+                    ((RichTextBox)cont).ReadOnly = !canEdit;
+                }
+            }
+            ddOrganisers.Enabled = canEdit;
+            ddVenue.Enabled = canEdit;
+            datePickerSingle.Enabled = canEdit;
+            selectSpeakers1.Enabled = canEdit;
+        }
+
+        private void enableEditing()
+        {
+            _enableEditing(true);
+        }
+
+        private void disableEditing()
+        {
+            _enableEditing(false);
+        }
+
+        private void btnEdit_TextChanged(object sender, EventArgs e)
+        {
+            if (btnEdit.Text == EDIT)
+            {
+                btnEdit.BackColor = SystemColors.ActiveCaption;
+            }
+            else if (btnEdit.Text == SAVE)
+            {
+                btnEdit.BackColor = Color.LimeGreen;
             }
         }
     }
