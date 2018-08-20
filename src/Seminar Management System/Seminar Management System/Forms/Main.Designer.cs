1a35a6aaef9ac3985c7551067d810adbdafbd00b
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-08-20 13:26:51 +1000

    Make main screen full screen on launch

--- src/Seminar Management System/Seminar Management System/Forms/Main.Designer.cs
+++ src/Seminar Management System/Seminar Management System/Forms/Main.Designer.cs
@@ -170,6 +170,7 @@
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Main_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
