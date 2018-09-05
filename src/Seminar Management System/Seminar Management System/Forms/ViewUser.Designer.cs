b9ed483b0f3766b26c7d0f504cc3f4da48782e37
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-09-05 14:17:14 +1000

    Implement DeleteUser

--- src/Seminar Management System/Seminar Management System/Forms/ViewUser.Designer.cs
+++ src/Seminar Management System/Seminar Management System/Forms/ViewUser.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.tbName = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbName
@@ -41,11 +44,51 @@
             this.tbName.TabIndex = 3;
             this.tbName.Text = "First Name Last Name";
             // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.ForeColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(692, 70);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Visible = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.Red;
+            this.btnCancel.Location = new System.Drawing.Point(692, 41);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Visible = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.Location = new System.Drawing.Point(692, 12);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
             // ViewUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(779, 535);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.tbName);
             this.Name = "ViewUser";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -59,5 +102,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnEdit;
     }
 }
--- src/Seminar Management System/Seminar Management System/Forms/ViewUser.cs
+++ src/Seminar Management System/Seminar Management System/Forms/ViewUser.cs
@@ -23,6 +23,8 @@ namespace Seminar_Management_System.Forms
             userReference = user;
         }
         private User userReference { get; set; }
+        private const string EDIT = "Edit";
+        private const string SAVE = "Save";
 
         private void ViewUser_Load(object sender, EventArgs e)
         {
@@ -33,6 +35,63 @@ namespace Seminar_Management_System.Forms
             if (userReference != null)
             {
                 tbName.Text = userReference.Name;
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (btnEdit.Text == EDIT)
+            {
+                enableEditing();
+                btnEdit.Text = SAVE;
+                btnCancel.Visible = true;
+                btnDelete.Visible = true;
+            }
+            else if (btnEdit.Text == SAVE)
+            {
+                disableEditing();
+                btnEdit.Text = EDIT;
+                btnCancel.Visible = false;
+                btnDelete.Visible = false;
+            }
+        }
+
+        private void _enableEditing(bool canEdit)
+        {
+            // param canEdit: true refers to enabling on
+            //                false refers to enabling off
+            foreach (var cont in this.Controls)
+            {
+                if (cont.GetType() == typeof(TextBox))
+                {
+                    ((TextBox)cont).ReadOnly = !canEdit;
+                }
+                else if (cont.GetType() == typeof(RichTextBox))
+                {
+                    ((RichTextBox)cont).ReadOnly = !canEdit;
+                }
+            }
+        }
+
+        private void enableEditing()
+        {
+            _enableEditing(true);
+        }
+
+        private void disableEditing()
+        {
+            _enableEditing(false);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this user?\nThis action can't be reversed.",
+                "Delete User",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                DataInstance.users.Remove(userReference);
+                this.Close();
             }
         }
     }
