499fec80da7933df84de9033905562f011caa5d5
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-09-22 10:40:10 +1000

    Add comments to SelectSpeaker

--- src/Seminar Management System/Seminar Management System/Custom Controls/SelectSpeakers.cs
+++ src/Seminar Management System/Seminar Management System/Custom Controls/SelectSpeakers.cs
@@ -10,12 +10,15 @@ using System.Windows.Forms;
 using Seminar_Management_System.Classes.Users;
 namespace Seminar_Management_System.Custom_Controls
 {
+    // This is used to display all the speakers
+        // Allows the user to select multiple speakers
     public partial class SelectSpeakers : UserControl
     {
         public SelectSpeakers()
         {
             InitializeComponent();
         }
+        // Expose the currently selected speakers
         public List<Speaker> SelectedSpeakers
         {
             get
@@ -29,14 +32,17 @@ namespace Seminar_Management_System.Custom_Controls
             }
         }
 
+        // Load a list of selected speakers and have it reflect in the interface
         public void setSpeakers(List<Speaker> speakers)
         {
             int index = 0;
             foreach (Speaker speaker in buildSpeakerListFromCheckedListBox(clbSpeakers.Items))
             {
                 clbSpeakers.SetItemChecked(index, false);
+                // Check if the current speaker is in the list provided by the user 
                 if (speakers.Contains(speaker))
                 {
+                    // Mark the speaker as checked
                     clbSpeakers.SetItemChecked(index, true);
                 }
                 index++;
@@ -55,6 +61,7 @@ namespace Seminar_Management_System.Custom_Controls
         }
         private void SelectSpeakers_Load(object sender, EventArgs e)
         {
+            // Load all the Speakers and set them to unchecked
             foreach (Speaker speaker in DataInstance.speakers)
             {
                 clbSpeakers.Items.Add(speaker.Name, false);
