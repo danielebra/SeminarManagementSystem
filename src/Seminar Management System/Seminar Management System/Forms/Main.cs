89d1e3c7e4bb03c17f445e89f94abdff43d3f24a
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-09-22 11:12:42 +1000

    Add comments to Main

--- src/Seminar Management System/Seminar Management System/Forms/Main.cs
+++ src/Seminar Management System/Seminar Management System/Forms/Main.cs
@@ -16,16 +16,20 @@ using System.IO;
 
 namespace Seminar_Management_System
 {
+    // This is the primary interface of the program
     public partial class Main : Form
     {
         public Main()
         {
             InitializeComponent();
         }
-
+        // The current Seminar items displayed in the interface
         private List<SeminarItem> seminarItems = new List<SeminarItem>();
+        // The current User items displayed in the interface
         private List<UserItem> userItems = new List<UserItem>();
+
         private InterfaceUnlocker interfaceUnlocker = new InterfaceUnlocker();
+
         private void btnAddSeminar_Click(object sender, EventArgs e)
         {
             AddSeminar addSeminar = new AddSeminar();
@@ -34,7 +38,10 @@ namespace Seminar_Management_System
 
         private void Main_Load(object sender, EventArgs e)
         {
+            // Give the DataInstance access to the current instance of this form
             DataInstance.mainInstance = this;
+            
+            // Look for the connection string to connect to the AWS database
             if (File.Exists("secrets.txt"))
             {
                 using (StreamReader stream = new StreamReader("secrets.txt"))
@@ -48,8 +55,11 @@ namespace Seminar_Management_System
                     "Unable to configure database connection", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
                     Environment.Exit(1);
             }
+            // Subscribe to seminar changes
             DataInstance.seminars.CollectionChanged += ObSeminars_CollectionChanged;
+            // Subscribe to user changes
             DataInstance.users.CollectionChanged += Users_CollectionChanged;
+            // Load data into the program
             try
             {
                 DataInstance.populateWithMockData();
@@ -63,6 +73,7 @@ namespace Seminar_Management_System
 
             // Fire resize
             Main_Resize(null, null);
+            // Begin reacting to logged in user changes
             interfaceUnlocker.Watch();
 
         }
@@ -77,6 +88,7 @@ namespace Seminar_Management_System
             DrawSeminarInterface();
         }
 
+        // Create the SeminarList interface
         private void DrawSeminarInterface()
         {
             for (int i = pnlSeminarView.Controls.Count - 1; i >= 0; i--)
@@ -92,6 +104,7 @@ namespace Seminar_Management_System
             {
                 SeminarItem seminarItem = new SeminarItem();
                 seminarItem.Location = new Point(0, seminarItem.Size.Height * seminarItems.Count);
+                // Change every second Seminar's color
                 if (seminarItems.Count % 2 == 1)
                     seminarItem.BackgroundColor = SystemColors.ActiveCaption;
 
@@ -102,6 +115,7 @@ namespace Seminar_Management_System
             }
         }
 
+        // Create the UserList interface
         private void DrawUserInterface()
         {
             for (int i = pnlUserView.Controls.Count - 1; i >= 0; i--)
@@ -117,20 +131,22 @@ namespace Seminar_Management_System
                 userItem.Location = new Point(0, userItem.Size.Height * userItems.Count);
                 var userInstance = user;
                 userItem.Populate(ref userInstance);
+                // Check for repeated users of the same type
                 if (userItems.Count > 0 && String.Equals(userItems.Last().UserReference.Role.Name, user.Role.Name))
                 {
+                    // Decide if the UserItem should be displayed with a shade of the same color
                     if (!userItems.Last().isUsingAlternativeColor)
                         userItem.AlternativeColor();
                 }
                 userItems.Add(userItem);
                 pnlUserView.Controls.Add(userItem);
-
             }
         }
 
         Random rnum = new Random();
         private void btnTest_Click(object sender, EventArgs e)
         {
+            // Add a random seminar to the list
             Seminar seminar = new Seminar();
             seminar.Title = "Created by test button";
             seminar.Description = rnum.Next(1111111, 1111111111).ToString();
@@ -166,6 +182,7 @@ namespace Seminar_Management_System
         
         private void Main_Resize(object sender, EventArgs e)
         {
+            // Ask each interface item to resize themselzes
             foreach (SeminarItem seminar in seminarItems)
                 seminar.Resize();
             foreach (UserItem user in userItems)
@@ -174,25 +191,30 @@ namespace Seminar_Management_System
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            // Show the LoginScreen
             LoginScreen loginScreen = new LoginScreen();
             loginScreen.Show();
         }
 
         private void btnAddUser_Click(object sender, EventArgs e)
         {
+            // Show the CreateAccount screen
             CreateAccount ca = new CreateAccount();
             ca.Show();
         }
 
         private void btnLaunchFilter_Click(object sender, EventArgs e)
         {
+            // Show the CreateFilter screen
             CreateFilter filt = new CreateFilter();
+            // Subscribe to the filter changes
             filt.FilterUpdated += Filt_FilterUpdated;
             filt.Show();
         }
 
         private void Filt_FilterUpdated(object sender, EventArgs e)
         {
+            // Re-draw the SeminarList interface when the filter has changed
             DrawSeminarInterface();
         }
     }
