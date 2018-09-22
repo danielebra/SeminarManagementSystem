0652147a71ad2263e80af4a7ab8f4d9bdd68d0c4
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-09-22 11:17:27 +1000

    Add comments to RegisterAttendee

--- src/Seminar Management System/Seminar Management System/Forms/RegisterAttendee.cs
+++ src/Seminar Management System/Seminar Management System/Forms/RegisterAttendee.cs
@@ -12,6 +12,8 @@ using Seminar_Management_System.Classes;
 using System.Reflection;
 namespace Seminar_Management_System.Forms
 {
+    // This is used to gather the relevant information for making an Attendee
+    // and creating the Attendee object
     public partial class RegisterAttendee : Form
     {
         public RegisterAttendee()
@@ -24,15 +26,20 @@ namespace Seminar_Management_System.Forms
             seminarReference = seminar;
         }
         private Seminar seminarReference { get; set; }
+        // Event used to alert that an Attendee has been registered
         public event EventHandler AttendeeRegistered;
 
         private void RegisterAttendee_Load(object sender, EventArgs e)
         {
+            // Create the interface on load
             CreateInterface();
         }
 
         public void CreateInterface()
         {
+            // Create interface based on the properties of a SeminarAttendee
+            // This is dynamically done inorder to make it easier to change what defines an attendee
+            // from a data structure perspective
             int counter = 0;
             int x = 5;
             foreach (PropertyInfo p in typeof(SeminarAttendee).GetProperties())
@@ -57,15 +64,20 @@ namespace Seminar_Management_System.Forms
         
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            // Get all the TextBoxes from the screen
             IEnumerable<Control> tbs = Utils.GetControlsFromControl(this, typeof(TextBox));
             List<string> vals = new List<string>();
             foreach (var cont in tbs)
                 vals.Add(((TextBox)cont).Text);
+            // Add the new Attendee to the Seminar's list of Attendees
             seminarReference.Attendees.Add(new SeminarAttendee(seminarReference.Attendees.Count,vals[0], vals[1], vals[2]));
+
             if (this.AttendeeRegistered != null)
             {
+                // Fire an event to alert subscribers that a change has occured in the AttendeeList
                 this.AttendeeRegistered(this, new EventArgs());
             }
+            // Close this interface
             this.Close();
 
         }
