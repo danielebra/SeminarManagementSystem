1774ef3019f49ddce9c4a99f7c01364a61d2a01b
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-09-22 11:03:18 +1000

    Add comments to CreateFilter

--- src/Seminar Management System/Seminar Management System/Forms/CreateFilter.cs
+++ src/Seminar Management System/Seminar Management System/Forms/CreateFilter.cs
@@ -12,12 +12,14 @@ using System.Linq.Expressions;
 
 namespace Seminar_Management_System.Forms
 {
+    // This is used to filter the Seminar list
     public partial class CreateFilter : Form
     {
         public CreateFilter()
         {
             InitializeComponent();
         }
+        // Fire an event when the filter has been updated
         public event EventHandler FilterUpdated;
 
         private void btnDone_Click(object sender, EventArgs e)
@@ -35,9 +37,13 @@ namespace Seminar_Management_System.Forms
     // This will be moved to its own file in the future
     public static class PortableFilter
     {
-        public static bool ByRoom { get; set; }
-        public static Room Room { get; set; }
+        // Each section will have a flag followed by a value
 
+        // Search by room
+        public static bool ByRoom { get; set; } // Enable or Disable searching by Room
+        public static Room Room { get; set; } // Which Room to search for
+
+        // Returns a list of Seminars that matches the search conditions defined by the properties above
         public static List<Seminar> Execute()
         {
             var query = from s in DataInstance.seminars
