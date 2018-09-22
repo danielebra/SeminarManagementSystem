b6ff3fe9da2842161878272f8504ee8664f093f2
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-09-22 10:10:56 +1000

    Comment Authentication

--- src/Seminar Management System/Seminar Management System/Classes/Authentication.cs
+++ src/Seminar Management System/Seminar Management System/Classes/Authentication.cs
@@ -8,6 +8,7 @@ namespace Seminar_Management_System.Classes
 {
     static class Authentication
     {
+        // All the Roles within this system
         public static List<Role> Roles = new List<Role>()
         {
             { new Role(Role.Names.Attendee, 0) },
@@ -16,14 +17,22 @@ namespace Seminar_Management_System.Classes
             { new Role(Role.Names.Organiser, 3) },
             { new Role(Role.Names.Admin, 5) }
         };
+
+        // Some helper functions for looking up Role data
+
+        // Search for a Role by its Name
         public static Role GetRoleFromName(string name)
         {
             return Roles.Where(r => r.Name == name).First();
         }
+
+        // Search for a Role by its Privilege level
         public static Role GetRoleFromPrivilegeLevel(int privilege)
         {
             return Roles.Where(r => r.Privilege == privilege).First();
         }
+
+        // Get Privilege level by Role Name
         public static int GetPrivilegeFromRoleName(string name)
         {
             return Roles.Where(r => r.Name == name).First().Privilege;
