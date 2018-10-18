415f243d4562cc1f0bede7e47bc80e23c8fba858
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-10-19 05:51:01 +1100

    Add Unit Tests

---
+++ src/Seminar Management System/SeminarManagementSystemTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeminarManagementSystemTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeminarManagementSystemTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13a77f17-9d6a-4cc3-b1bd-8c0efffde9db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Seminar Management System/SeminarManagementSystemTests/UnitTest1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeminarManagementSystemTests
+{
+    [TestClass]
+    public class Database
+    {
+        [TestMethod]
+        public void ConnectToDatabase()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void ReadFromDatabase()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void AddToDatabase()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void RemoveFromDatabase()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void DatabaseRespondsWithin2Seconds()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+    [TestClass]
+    public class Interface
+    {
+        [TestMethod]
+        public void LoadUsers()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadSeminars()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadLogin()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadCreateUser()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadCreateSeminar()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadFilter()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadSpeakerDetails()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoadRegisterAttendee()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+    [TestClass]
+    public class DataTransition
+    {
+
+        [TestMethod]
+        public void RegisterAttendee()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void LoginUser()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void AssignSpeaker()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ChangeSpeaker()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void DeleteSeminar()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LoginAsAttendee()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Logout()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
